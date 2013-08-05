8fa09458d1e0b2419b053031b0364a3a7970c99d
suisho <suisho000000@gmail.com>
2013-08-06 02:31:03 +0900

    とりあえずparseできた

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 var cheerio = require('cheerio')
 var request = require('request')
 var Buffer  = require('buffer').Buffer
-var encoding = require('./Unzipper.js/src/encoding.js')
-var Iconv = require('iconv').Iconv
-function getFlavor(callback){
+var Iconv   = require('iconv').Iconv
+
+function getPage(callback){
   var flavorUrl = "http://www.31ice.co.jp/contents/product/flavor/index.html"
-  request(flavorUrl, function(err, response, body){
-    var conv = new Buffer(response.body)
+  request({ url : flavorUrl, encoding :null }, function(err, response, body){
+    var conv = response.body
     var iconv = new Iconv('EUC-JP','UTF-8//TRANSLIT//IGNORE')
     conv = iconv.convert(conv)
     conv = conv.toString()
-    console.log(conv);
     //console.log(encoding.Encoding.isEUCJP(response.body))//.join(""));
-    parse(conv)
-    callback()
+    callback(conv)
   })
 }
 
-function parse(html){
+function getFlavor(callback){
+  getPage(function(body){
+    callback(null, parseFlavors(body))
+  })
+}
+
+function parseFlavors(html){
   var $ = cheerio.load(html)
   var names = []
+  // fravor for month
+  names.push($("#fomName").text())
+  // season flavor
   $('#seasonFlavorPhoto img').each(function(){
     var name = $(this).attr("alt")
     names.push(name)
-    console.log(name);
-    return name
   })
+  return names
 }
 
 module.exports = function(callback){
--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 describe('cotton candy!', function () {
   it('get flavor', function (done) {
     var cc = require('../index.js')
-    cc(done)
+    cc(function(err, name){
+      console.log(name);
+      done()
+    })
   });
 });
